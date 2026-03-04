404f9b69ce881214f872240376be995f634a2513
Theo YOSHIURA <theo.yoshiura@brassart.fr>
2026-03-04 13:31:30 +0100

    j'ai fait des choses askip

--- Assets/Scripts/ScriptableObjects/Character.cs
+++ Assets/Scripts/ScriptableObjects/Character.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Character", menuName = "ScriptableObjects/Character")]
 public class Character : ScriptableObject
 {
+    [SerializeField] private Sprite _spriteImage;
     [SerializeField] private string _name;
     [SerializeField] private Statistics _statistics;
     [SerializeField] private Weapon _weapon;
@@ -12,7 +13,9 @@ public class Character : ScriptableObject
     [SerializeField] private Chestplate _chesplate;
     [SerializeField] private Leggings _leggings;
     [SerializeField] private Boots _boots;
+    [SerializeField] private Spell[] _spells;
 
+    public Sprite SpriteImage { get => _spriteImage; set => _spriteImage = value; } 
     public string Name { get => _name; set => _name = value; }
     public Statistics Statistics { get => _statistics; set => _statistics = value; }
     public Weapon Weapon { get => _weapon; set => _weapon = value; }
@@ -20,4 +23,5 @@ public class Character : ScriptableObject
     public Chestplate Chesplate { get => _chesplate; set => _chesplate = value; }
     public Leggings Leggings { get => _leggings; set => _leggings = value; }
     public Boots Boots { get => _boots; set => _boots = value; }
+    public Spell[] Spells { get => _spells; set => _spells = value; }
 }
--- Assets/Scripts/ScriptableObjects/Enemy.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableObjects/Enemy")]
-public class Enemy : Character
-{
-
-}
